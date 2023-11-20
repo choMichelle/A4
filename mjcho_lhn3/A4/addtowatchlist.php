ab64d48c42e84b4f4f252e8b9dd1d00261fd3b73
LongNguyen <hoanglongst2000@gmail.com>
2023-11-20 15:39:19 -0800

    try to create addtowatchlist

--- mjcho_lhn3/A4/addtowatchlist.php
+++ mjcho_lhn3/A4/addtowatchlist.php
@@ -6,6 +6,18 @@ include("header.php");
 // SSLtoHTTP();
 ?>
 
+<?php
+if (isset($_SESSION['email'])) {    
+
+    //Create a table for watchlist with useremail and model column
+    $allInputValid = false;
+    $create_table_query = "CREATE TABLE IF NOT EXISTS `classicmodels`.`watchlist` (`email` VARCHAR(255) NOT NULL , `hashedPassword` VARCHAR(255) NOT NULL , PRIMARY KEY (`email`)) ENGINE = InnoDB";
+    mysqli_query($db, $create_table_query);
+}
+
+
+?>
+
 <html lang="en">
     <head>
 
